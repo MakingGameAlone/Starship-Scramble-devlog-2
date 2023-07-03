bce622ded37dd5e0b07ccee2aaa88f288fc131a7
KultrasHub <Kultras1808@gmail.com>
2023-07-03 22:11:45 +0700

    study case

---
+++ starship-scramble-devlog-2/src/Component/StudyComp/StudyParagraph.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './StudyParagraph.css'
+
+function StudyParagraph() {
+    return (
+        <div className='studyparagraph-container'>
+            <h4>Origin</h4>
+
+            <p>I didn't start this project as dream game, in fact, I was developping the dream game but I took departure from the road and start this development, due to computer issue.
+                I didn't expect to build a full game at first, however, the game idea kept me interested after I resolved the computer's problem. 
+            </p>
+
+            <p>
+                I chose the Arena shooter roguelike genre for this game. One major reason is workload, I am undergraduatee that must take presure from university and work; plus the fact that arena shooter roguelike genre is much smaller in scope.
+                The game have been underdevelopment for nearly 1 year and I am happy with the current result and my hard work.
+            </p>
+
+            <div className='divider'></div>
+
+            <h4>Case Study 1: Soulstone survivor</h4>
+            <p>To be honest, I don't quite like this game, however, I have spent over 30 hours in this game due to the grinding system. </p>
+
+            <p>    At the start, I notice the issue with this game VFX, every thing is flashy and awesome which really makes nothing flashy and awesome.
+                 Different characters are enjoyable, but I notice my favourites are the ones who have subtle vfx and therefore, the vfx I selected in Starship Scramble are much visible and comprehensive.
+                 Secondly, I enjoy the color selection for each type of element. It create a sense of order in such a chaos game! I decided to reform the arsenal system followed this example.
+            </p>
+            
+            <div className='divider'></div>
+
+            <h4>Case Study 2: Hades</h4>
+            <p>One of my favorite videogame, I spent around 100 hours to complete every single achievement.</p>
+            <p>Hades is my source of inspiration for multiple-resource system, construction and upgrade. Different resources are used for different purpose, which allows players options based on their need.
+            </p>
+            <p>
+                Mirror of Night is a stats upgrade system in Hades and it is amazing. Normally, in any other games, I would simply pump the money to all the stats (becase I can); however, Mirror of night requires players to choose which stats for the next run.
+                A feature in roguelike is choice and to force players strategicly think to win long term and shorterm is totally an achievement in game design! Therefore, I copy and paste it into the game with certain twist and exaggeration.
+            </p>
+
+            <div className='divider'></div>
+
+            <h4>Case Study 3: Brotato</h4>
+            <p>I haven't tried this game! Yet my radar still catches the strange popularity of this game. </p>
+        </div>
+    )
+}
+
+export default StudyParagraph
--- starship-scramble-devlog-2/src/Page/Study.js
+++ starship-scramble-devlog-2/src/Page/Study.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PageHeader from '../Component/PageHeader';
 import Footer from '../Component/Footer';
+import StudyParagraph from '../Component/StudyComp/StudyParagraph';
 
 function Study() {
   return (
@@ -10,6 +11,7 @@ function Study() {
         title = {"Study and Research"}
         subtitle = {"My motivation to start the projects and other studies along the way,"}
     />
+    <StudyParagraph />
     <Footer />
 </>
   )
